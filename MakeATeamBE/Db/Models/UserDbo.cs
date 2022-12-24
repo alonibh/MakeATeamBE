c2a5194d840f0e243c961d2e3abb30052611fa09
Alon Ben Horin <alonibh@gmail.com>
2022-12-24 21:45:11 +0200

    Add comment

--- MakeATeamBE/Db/Models/UserDbo.cs
+++ MakeATeamBE/Db/Models/UserDbo.cs
@@ -5,6 +5,7 @@ namespace MakeATeamBE.Db.Models
 {
     public class UserDbo
     {
+        // TODO change to int/long
         [Key]
         public string Id { get; set; }
         public string Name { get; set; }
